b33d81710bdd31aa6b3e1245c10061c845be1ea1
ZPLOVEXYH <1031626336@qq.com>
2019-12-10 16:21:45 +0800

    1、添加网关路由以及监控插件，可以http访问查看到beans和所有的配置信息

--- spring-cloud-gateway/src/main/java/com/spring/cloud/gateway/SpringCloudGatewayApplication.java
+++ spring-cloud-gateway/src/main/java/com/spring/cloud/gateway/SpringCloudGatewayApplication.java
@@ -2,9 +2,6 @@ package com.spring.cloud.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.gateway.route.RouteLocator;
-import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
-import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
@@ -14,14 +11,4 @@ public class SpringCloudGatewayApplication {
     public static void main(String[] args) {
         SpringApplication.run(SpringCloudGatewayApplication.class, args);
     }
-
-    @Bean
-    public RouteLocator myRoute(RouteLocatorBuilder builder) {
-        return builder.routes()
-                .route(p -> p.path("/new")
-                        .filters(f -> f.addRequestHeader("Authorization", "Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJ6cCIsImlzcyI6Imh0dHA6Ly93d3cuc2FtcGxlLmNuIiwiaWF0IjoxNTc1MzM5ODM0fQ.gME4q_IlprrOTsGkVBEOQe01jmglirOqHWTx33vBiyn_VA4jchmfz19hFvatH6rgAbN5PMiJL_BydL93MUfoXw"))
-                        .uri("http://192.168.43.101:8088/api/CRoles/")
-                ).build();
-    }
-
 }
