95aab33e5085e4b563123fb8660b15d327fc8a04
ZPLOVEXYH <1031626336@qq.com>
2019-12-12 09:14:53 +0800

    1、添加微服务网关过滤器和全局过滤器。

--- spring-cloud-gateway/src/main/java/com/spring/cloud/gateway/SpringCloudGatewayApplication.java
+++ spring-cloud-gateway/src/main/java/com/spring/cloud/gateway/SpringCloudGatewayApplication.java
@@ -1,7 +1,9 @@
 package com.spring.cloud.gateway;
 
+import com.spring.cloud.gateway.filter.TokenFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
@@ -10,5 +12,13 @@ public class SpringCloudGatewayApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringCloudGatewayApplication.class, args);
+    }
+
+    //    @Autowired
+//    TokenFilter filter;
+//
+    @Bean
+    public TokenFilter tokenFilter() {
+        return new TokenFilter();
     }
 }
---
+++ spring-cloud-gateway/src/main/java/com/spring/cloud/gateway/controller/DemoController.java
@@ -0,0 +1,16 @@
+package com.spring.cloud.gateway.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class DemoController {
+
+    @GetMapping("/cUsers")
+    public String demo() {
+        return "test";
+    }
+
+}
